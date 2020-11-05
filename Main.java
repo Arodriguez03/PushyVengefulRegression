eaf4c9807fe86bf292f1bbb84897b5be17f61f27
Alejandro Rodriguez <alejandro.rodriguez@patelhs.org>
2020-11-05 17:58:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+class Main {
+  public static void main(String[] args) {
+    //Create an array of 15 int elements (representative of the seat #s which we DO know
+    int[] classSeats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    //Create a String array of 15 elements (representative of the student names which we do NOT know)
+    String[] studentNames = new String[15];
+    //Use a For loop to loop through the String array
+      for(int i = 0; i < studentNames.length; i++){
+        //Within the For loop, you will want to ask for the student's name (print statement), receive the name(Scanner) and assign it to each element of the String array
+
+        
+      }
+  }
+}
